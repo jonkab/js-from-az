fb1f3827cdaab1bce034071aead814b42c0c2079
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-10-22 22:57:08 +0200

    Ajout exercice sur "Variables et types"

--- 1.Les-bases-de-JavaScript/Exercices/1.Variables-et-types/script.js
+++ 1.Les-bases-de-JavaScript/Exercices/1.Variables-et-types/script.js
@@ -1,15 +1,20 @@
 /* 
-    1. Créez une variable let nommée title et donnez-lui une valeur de "Top 10 des meilleures cartes graphiques." 
+    1. Créez une variable let nommée title et donnez-lui une valeur
+    de "Top 10 des meilleures cartes graphiques."
 */
-
+let title = "Top 10 des meilleures cartes graphiques.";
+console.log(title);
 
 /* 
     2. Créez une constante nommée id et donnez-lui une valeur de type nombre. 
 */
-
-
+const id = 10;
+console.log(typeof id);
 
 /* 
-    3. Créez une variable temperature avec let sans l'initialiser, puis assignez-lui une valeur à la ligne suivante. 
+    3. Créez une variable temperature avec let sans l'initialiser, puis
+    assignez-lui une valeur à la ligne suivante.
 */
-
+let temperature;
+temperature = 12;
+console.log(temperature);

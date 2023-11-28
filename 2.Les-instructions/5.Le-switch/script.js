bcc3a0350efaa3159de1603b8c5af430a92a7279
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-11-28 17:08:58 +0100

    Ajout du cours sur "Le switch"

--- 2.Les-instructions/5.Le-switch/script.js
+++ 2.Les-instructions/5.Le-switch/script.js
@@ -8,3 +8,44 @@
 */
 
 const tShirtSize = "M";
+
+switch (tShirtSize) {
+    case "XL":
+        console.log("Prix : 19,99€");
+        break;
+    case "L":
+        console.log("Prix : 17,99€");
+        break;
+    case "M":
+        console.log("Prix : 15,99€");
+        break;
+    case "S":
+        console.log("Prix : 13,99€");
+        break;
+    default:
+        console.log("Taille incorrecte.")
+}
+
+/* Version en créant une fonction
+function tShirtPrice (tShirtSize) {
+
+    switch (tShirtSize) {
+        case "XL":
+            return "Prix : 19,99€";
+            break;
+        case "L":
+            return "Prix : 17,99€";
+            break;
+        case "M":
+            return "Prix : 15,99€";
+            break;
+        case "S":
+            return "Prix : 13,99€";
+            break;
+        default:
+            return "Taille incorrecte.";
+    }
+}
+
+console.log(tShirtPrice("M"));
+*/

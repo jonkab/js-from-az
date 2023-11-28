cd42997cf52985ad9a6d44e96388aede7691041d
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-11-28 20:41:58 +0100

    Ajout du cours sur "Opérateur ternaire"

--- 2.Les-instructions/6.Operateur-ternaire/script.js
+++ 2.Les-instructions/6.Operateur-ternaire/script.js
@@ -18,6 +18,16 @@ const passenger2 = {
   premium: false
 }
 
+//Ternaire écrite dans une fonction avec la syntaxe standard
+function isAdult1(passenger) {
+  return passenger.age  >= 18 ? true : false;
+}
+console.log(isAdult1(passenger1));
+
+//Ternaire écrite dans une fonction fléchée
+const isAdult2 = thePassengers => thePassengers.age >= 18 ? true : false;
+console.log(isAdult2(passenger1));
+
 
 
 
@@ -25,6 +35,6 @@ const passenger2 = {
 // Attention, n'utilisez que les opérations pour des calculs courts.
 // On peut en effet les chaîner, ce qui donne un résultat affreux.
 // Ne faites pas ça.
-
+/*
 let generation = passenger1.age >= 0 && passenger1.age <= 20 ? "Premier âge" : passenger1.age > 20 && passenger1.age <= 40 ? "Second âge" : passenger1.age > 40 && passenger1.age <= 60 ? "Troisième age" : "Quatrième age";
-console.log(generation);
+console.log(generation);*/

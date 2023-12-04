4be114a18aab8470cbd3704d7c080cbc4f63f61c
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-12-04 10:58:50 +0100

    Ajout du cours sur "For...In et For...Of"

--- 2.Les-instructions/10.for...in-et-for...of/script.js
+++ 2.Les-instructions/10.for...in-et-for...of/script.js
@@ -1,12 +1,13 @@
-/* 
+/**
     1. Les boucles modernes for...in et for...of.
 
     for...in sert à itérer à travers des objets.
 
-    for...of sert à itérer à travers des éléments itérables, comme les tableaux ou les chaînes de caractères.
+    for...of sert à itérer à travers des éléments itérables, comme les
+    tableaux ou les chaînes de caractères.
 */
 
-
+//Exemple pour "for...in"
 const person = {
   name: "Cyril",
   age: 35,
@@ -14,7 +15,18 @@ const person = {
   weight: 90
 }
 
+for (const prop in person) {//Ici "prop" renvoi les propriétés (leurs noms)
+  console.log(prop, person[prop]);//Voici la syntaxe pour afficher soit
+  //leurs noms, soit leurs valeurs.
+}
+//Explication sur l'usage de la bracket notation [] :
+//A vérifier mais ce qui expliquerai qu'on utilise les [] serait que
+//"prop" est une chaîne de caractères
 
 
+//Exemple pour "for...of"
 const fruits = ["🍓","🍇","🍑","🍎","🥭"]
 
+for (const fruit of fruits) {//Ici "fruit" stock les valeurs du tableaux
+  console.log(fruit);//On peut donc itérer sur le tableau et afficher les valeurs
+}

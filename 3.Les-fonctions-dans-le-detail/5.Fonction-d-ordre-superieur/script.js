c208db01cd8f6c28953df1e40956ffd86f5c8a03
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-12-05 16:44:05 +0100

    Ajout du cours sur "Les fonctions d'ordre supérieur"

--- 3.Les-fonctions-dans-le-detail/5.Fonction-d-ordre-superieur/script.js
+++ 3.Les-fonctions-dans-le-detail/5.Fonction-d-ordre-superieur/script.js
@@ -1,15 +1,41 @@
-/* 
-    1. Fonction d'ordre supérieur.
+/**
+    1. Fonctions d'ordre supérieur.
 
-    Les fonctions d'ordre supérieur sont des fonctions qui vont soit recevoir une fonction callback en argument soit retourner une fonction, ou les deux à la fois !
+    Les fonctions d'ordre supérieur sont des fonctions qui vont soit recevoir
+    une fonction callback en argument, soit retourner une fonction,
+    ou les deux à la fois !
 
     Cela ouvre la voie à de nombreuses solutions et à une plus grande fléxibilité.
-*/
 
-// codons une fonction .filter() personnalisée
+    Voici un exemple en créant une fonction personnalisée qui permet de filtrer
+    le montant des salaires selon un tableau les contenants puis le montant du
+    filtre désiré.
 
+    Comme ceci :
+    1- La fonction s'appelle customFilter()
+    2- La fonction prend 2 arguments en paramètre :
+        - un tableau
+        - une fonction callback
+    Ce qui donne la syntaxe : function customFilter (arr, callback) {}
 
-const salaries = [1200,5000,4000,2500,3450,1800]
+    ℹ️ Pour info :
+    Il existe déjà une fonction native JavaScript appelé ".filter()" qui fait la même chose.
+    Le but est justement d'en faire une personnaliser, car c'est un bon exercice
+    pour illustrer ce qu'est une fonction d'ordre supérieur (une callback).
+ */
+const salaries = [1200,5000,4000,2500,3450,1800];
+
+function customFilter (arr, callback) {
+    const filteredArray = [];
+    for (const arrElement of arr) {
+        if (callback(arrElement)) {
+            filteredArray.push(arrElement);
+        }
+    }
+
+    return filteredArray;
+}
+console.log(customFilter(salaries, amount => amount > 3000));
 
 
 const countries = [
@@ -34,25 +60,28 @@ const countries = [
         gdp: 1988
     },
 ]
+console.log(customFilter(countries, country => country.gdp > 2000));
 
-
-// Exemple avec un composant d'ordre supérieur
-
+//Exemple avec un composant d'ordre supérieur avec le framework React
+/**
 export default function Private() {
-    // On exécute useContext pour savoir si l'utilisateur est connecté.
+     //On exécute useContext pour savoir si l'utilisateur est connecté.
     const {currentUser} = useContext(UserContext)
   
-    // Si il n'est pas connecté, on retourne à la page d'accueil.
-    // <Navigate />, in fine, est une fonction. 
+     //Si il n'est pas connecté, on retourne à la page d'accueil.
+     //<Navigate />, in fine, est une fonction.
     if(!currentUser) {
-      return <Navigate to="/" />
+      return <Navigate to="/" />//Ici cette syntaxe en React retourne une fonction
     }
   
-    // Sinon on les emmène vers une page privé.
-    // <Outlet />, in fine, est une fonction. 
+     //Sinon on les emmène vers une page privé.
+     //<Outlet />, in fine, est une fonction.
     return (
       <div className="container">
-        <Outlet />
+        <Outlet />//Ici cette syntaxe en React retourne une fonction aussi
       </div>
     )
   }
+  //Donc là aussi, il s'agit bien d'une fonction d'ordre supérieur puisqu'elle
+  //retourne 2 fonctions
+*/

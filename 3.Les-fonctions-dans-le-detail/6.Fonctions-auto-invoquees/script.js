fa9624b76bd030fdc2da117685e5885d1a6bf097
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-12-06 15:49:35 +0100

    Ajout du cours sur "Les fonctions auto-invoquées (IIFEE)"

--- 3.Les-fonctions-dans-le-detail/6.Fonctions-auto-invoquees/script.js
+++ 3.Les-fonctions-dans-le-detail/6.Fonctions-auto-invoquees/script.js
@@ -1,11 +1,29 @@
-/* 
-    1. Les fonctions auto-invoquées.
+/**
+    1. Les fonctions auto-invoquées (IIFEE).
+    EN : Immediately Invoked Function Expression
+    FR : Expression de fonction invoquée immédiatement
 
-    Assez rarement utilisées, ce sont des fonctions qui s'exécutent dès qu'elles sont définies.
+    Assez rarement utilisées, ce sont des fonctions qui s'exécutent dès qu'elles
+    sont définies.
+
+    L'utilisation principale des IIFE était de créer un contexte isolé pour
+    éviter les collisions de noms de variables. Cependant, avec
+    l'introduction de let et const, qui ont une portée de bloc,
+    l'utilisation des IIFE a diminué. Cela dit, elles restent parfois
+    utiles dans certaines situations.
 
 */
 
 // Syntaxe de base 
+(function () {
+    console.log("Cette fonction est auto-invoquée !");
+})();//les parenthèses finales () invoquent immédiatement la fonction
 
+// Avant l'arrivée de let & const qui règlent les problèmes de portée, on pouvait
+// déclarer des var dans des IIFEE pour éviter de polluer la portée globale.
 
-// Avant l'arrivée de let & const qui règlent les problèmes de portée, on pouvait déclarer des var dans des IIFEE pour éviter de polluer la portée globale.
+//Avec "var" la variable est directement accessible depuis l'objet "window"
+var directInWindow = "directInWindow";
+//Avec "let" la variable n'est plus directement accessible depuis l'objet "window"
+let notDirectlyInWindow = "notDirectlyInWindow";
+console.log(window);

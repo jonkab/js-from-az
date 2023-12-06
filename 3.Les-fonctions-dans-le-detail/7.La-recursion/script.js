7e830ed8b872855910c7af31a15403fd7bf03c24
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2023-12-06 16:33:27 +0100

    Ajout du cours sur "La récursion"

--- 3.Les-fonctions-dans-le-detail/7.La-recursion/script.js
+++ 3.Les-fonctions-dans-le-detail/7.La-recursion/script.js
@@ -1,20 +1,27 @@
-/* 
+/**
     1. La récursion.
 
     La récursion est le comportement d'une fonction qui s'appelle elle-même.
 
-    Attention, la récursion est moins rapide qu'une boucle et en général moins simple à comprendre.
-*/
+    Attention, la récursion est moins rapide qu'une boucle et en général
+    moins simple à comprendre.
 
+    Exemple de récursion simple qui incrémente une variable "num" jusqu'à ce
+    qu'elle atteigne une valeur supérieure à 10.
+*/
 function recursion(num) {
     num++;
-    
-    if(num > 10) {
+    /** ⚠️Important ⚠️
+        Il est important d'avoir une condition d'arrêt qui évite une récursion infinie.
+        C'est une bonne pratique d'avoir une condition d'arrêt pour que la fonction
+        ne s'appelle plus elle-même à un moment donné.
+     */
+    if(num > 10) {//Condition d'arrêt qui évite la récursion
       console.log("Num est strictement supérieur à  10 !");
       console.log(num);
     }
-    else {
-      recursion(num)
+    else {//Appel dans la fonction de la fonction elle-même qui prend un argument
+      recursion(num)//ce qui génére la récursivité
     }
 }
-recursion(0);
+recursion(0);//Appel de la fonction pour la lancer et tester.

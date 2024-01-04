d57b3a1a76aad1cf7688fd79cfe2166581edfae6
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-04 10:16:04 +0100

    Ajout du cours sur "Trouver un élément ou sa présence" grâce aux méthodes : - indexOf() - findIndex() - includes() - some() - every() - find()

--- 5.Les-tableaux-dans-le-detail/5-Trouver-un-element-ou-sa-presence/script.js
+++ 5.Les-tableaux-dans-le-detail/5-Trouver-un-element-ou-sa-presence/script.js
@@ -2,7 +2,8 @@
     Il existe beaucoup de méthodes utiles pour trouver un élément, son index ou sa présence dans un tableau.
 */
 
-/* 
+console.log("------ 1. Array.prototype.indexOf(value) ------");
+/*
     1. Array.prototype.indexOf(value)
 
     Retourne l'index du premier élement qui correspond à l'argument.
@@ -10,8 +11,11 @@
 
 const cats = ["Poppy", "Misty", "Luna", "Lily", "Titi"];
 
+console.log(cats.indexOf("Luna"));//Retourne -> 2
 
-/* 
+
+console.log("\n\n ------ 2. Array.prototype.findIndex(callback(element, index, array)) ------");
+/*
     2. Array.prototype.findIndex(callback(element, index, array))
 
     Définition: Retourne l'index de l'élément qui, le premier, fait en sorte que la callback retourne true.
@@ -36,17 +40,21 @@ const germanCities = [
     },
 ]
 
+console.log(germanCities.findIndex(city => city.population > 3));// Retourne -> 0
 
 
-/* 
+console.log("\n\n ------ 3. Array.prototype.includes(value) ------");
+/*
     3. Array.prototype.includes(value)
 
     Retourne true ou false en fonction de la présence de l'argument fourni.
 */
 
+console.log(cats.includes("Lily"));// Retourne -> true
 
 
-/* 
+console.log("\n\n ------ 4. Array.prototype.some(callback(element, index, array)) ------");
+/*
     4. Array.prototype.some(callback(element, index, array))
 
     Définition: Teste tous les éléments d'un tableau avec une fonction callback.
@@ -75,18 +83,25 @@ const employees = [
     }
 ]
 
+console.log(employees.some(employe => employe.age > 40))// Retourne -> true
 
-/* 
+
+console.log("\n\n ------ 5. Array.prototype.every(callback(element, index, array)) ------");
+/*
     5. Array.prototype.every(callback(element, index, array))
 
     Définition: Retourne true seulement si la callback exécutée sur chaque élément retourne tout le temps true.
-    
+
 */
 
 const weights = [60,45,99,75,85,77,105];
+console.log(weights.every(weight => weight > 50));// Retourne -> false
+//Car sur les 7 nombres du tableau au moins un (45) est inférieur à 50
+//donc même si la majorité des nombres (6 sur 7) sont bien supérieur à 50 ça ne répond pas à 100% de la condition
+//ce qui empêche d'être "true" mais "false" à la place
 
 
-
+console.log("\n\n ------ 6. Array.prototype.find(callback(element,index,array)) ------");
 /*
     6. Array.prototype.find(callback(element,index,array))
 
@@ -96,22 +111,23 @@ const weights = [60,45,99,75,85,77,105];
 
 */
 
-    const users = [
-        {
-            id: 1,
-            name: "Paul"
-        },
-        {
-            id: 2,
-            name: "Michel"
-        },
-        {
-            id: 3,
-            name: "Laura"
-        },
-        {
-            id: 4,
-            name: "Antoine"
-        },
-    ]
+const users = [
+    {
+        id: 1,
+        name: "Paul"
+    },
+    {
+        id: 2,
+        name: "Michel"
+    },
+    {
+        id: 3,
+        name: "Laura"
+    },
+    {
+        id: 4,
+        name: "Antoine"
+    },
+]
 
+console.log(users.find(user => user.name === "Laura"));// Retourne -> { id: 3, name: 'Laura' }

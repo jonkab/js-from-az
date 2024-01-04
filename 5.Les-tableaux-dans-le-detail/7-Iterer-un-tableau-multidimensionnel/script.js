9e21f4e2bc4dcbb7c64cb6022a8edb9db27e5f5c
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-04 12:09:37 +0100

    Ajout du cours sur "Itérer un tableau multidimensionnel" grâce aux méthodes : - for() - for...of

--- 5.Les-tableaux-dans-le-detail/7-Iterer-un-tableau-multidimensionnel/script.js
+++ 5.Les-tableaux-dans-le-detail/7-Iterer-un-tableau-multidimensionnel/script.js
@@ -1,13 +1,30 @@
 /* 
-    1. Un tableau multidimensionnel.
+    Tableau multidimensionnel.
 
     Un tableau multidimensionnel à plusieurs niveaux.
 
-    Exemple: 
+    Exemple :
     const couples = [["Tom","Lea"], ["Jack", "Sara"], ["Pedro", "Maria"]];
 
-    On peut itérer à travers ce genre de tableau avec une double boucle for.
+    On peut itérer à travers ce genre de tableau avec :
+    - une double boucle for
+    - une double boucle for...of (nouvelle méthode depuis 2015)
 */
 
+console.log("------ 1. for(value) ------");
 const couples = [["Tom","Lea"], ["Jack", "Sara"], ["Pedro", "Maria"]];
 
+for (let i = 0; i < couples.length; i++) {
+    //console.log(couples[i]);
+    for (let j = 0; j < couples[i].length; j++) {
+        console.log(couples[i][j]);
+    }
+}
+
+
+console.log("\n\n------ 2. for...of ------");
+for (const arr of couples) {
+    for (const names of arr) {
+        console.log(names);
+    }
+}

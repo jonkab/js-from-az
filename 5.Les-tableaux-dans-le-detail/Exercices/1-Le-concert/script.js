9a70c6ebaabe6eb00eab4c8eb7ed4e5dd7bb9152
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-07 17:28:21 +0100

    Ajout exercices sur "Les tableaux"

--- 5.Les-tableaux-dans-le-detail/Exercices/1-Le-concert/script.js
+++ 5.Les-tableaux-dans-le-detail/Exercices/1-Le-concert/script.js
@@ -1,7 +1,9 @@
-/*  
-    1. Le concert.    
+console.log("----- Exercice [1] Le concert -----");
+/*
+    1. Le concert.
 
-    Vous allez devoir effectuer plusieurs opérations sur un tableau représentant une "foule" assistant à un concert, à vous d'utiliser les bonnes méthodes associées.
+    Vous allez devoir effectuer plusieurs opérations sur un tableau représentant une "foule"
+    assistant à un concert, à vous d'utiliser les bonnes méthodes associées.
 
     Tentez de résoudre les étapes ci-dessous une à une.
 */
@@ -29,21 +31,28 @@ const concertRoom = [
     },
 ]
 
-
+console.log("----- Le concert - A -----");
 /* 
     A. L'abonnement de Paul a expiré, vous devez l'exclure du concert.
 */
+//console.log(concertRoom.pop());
+console.log(concertRoom.splice(concertRoom.length - 1, 1));
+
+console.log(concertRoom);
 
 
-
+console.log("\n\n----- Le concert - B -----");
 /* 
-    B. Afin de mieux analyser votre base de données, triez les objets du tableau par rapport à leur âge dans l'ordre croissant.
+    B. Afin de mieux analyser votre base de données, triez les objets du tableau par rapport
+       à leur âge dans l'ordre croissant.
 */
+console.log("Trie par age croissant : ", concertRoom.sort((a,b) => a.age - b.age));
 
 
-
+console.log("\n\n----- Le concert - C -----");
 /* 
-    C. Les deux premieres personnes du tableau nouvellement trié, Adam et Johanna, veulent échanger leur place avec leur cousin Thomas et Clara.
+    C. Les deux premieres personnes du tableau nouvellement trié, Adam et Johanna, veulent échanger
+       leur place avec leur cousin Thomas et Clara.
 
     Remplacez les deux premiers éléments du tableau par : 
 
@@ -57,9 +66,14 @@ const concertRoom = [
     }
 */
 
+console.log("Adam et Johanna : ", concertRoom.splice(0, 2, { name: 'Thomas', age: 19 }, { name: 'Clara', age: 21 }))
+console.log("Echangé avec Thomas et Clara : ", concertRoom);
 
-
+console.log("\n\n----- Le concert - D -----");
 /* 
-    D. Le concert va commencer d'ici deux heures, envoyez dans la console un rappel à tous les participants sous cette forme : "Salut [name], le concert commencera d'ici deux heures !"
+    D. Le concert va commencer d'ici deux heures, envoyez dans la console un rappel à tous
+       les participants sous cette forme :
+        "Salut [name], le concert commencera d'ici deux heures !"
 */
 
+concertRoom.forEach( person => console.log( `Salut ${person.name}, le concert commencera d'ici deux heures !`));

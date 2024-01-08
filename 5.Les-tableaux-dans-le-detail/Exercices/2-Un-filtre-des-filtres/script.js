d19e99748331461d9412128dae3755146b7b7dee
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-08 10:12:42 +0100

    Ajout exercices 2 sur "Les tableaux"

--- 5.Les-tableaux-dans-le-detail/Exercices/2-Un-filtre-des-filtres/script.js
+++ 5.Les-tableaux-dans-le-detail/Exercices/2-Un-filtre-des-filtres/script.js
@@ -4,11 +4,12 @@
     Mettez votre chapeau de filtreur en chef et résolvez ces différents problèmes.
 */
 
-
+console.log("----- Exercice A : Filtrez les entreprises revenue > 500 milliards -----");
 /* 
-    A. Vers l'infini et l'au delà.
+    A. Vers l'infini et l'au-delà.
 
-    Filtrez les entreprises dont le revenue annuel est supérieur à 500 milliards de dollars dans un nouveau tableau appelé "filteredCompanies" et retournez le dans la console.
+    Filtrez les entreprises dont la revenue annuelle est supérieur à 500 milliards de dollars dans un nouveau
+    tableau appelé "filteredCompanies" et retournez-le dans la console.
 */
 
 const companies = [
@@ -36,26 +37,33 @@ const companies = [
     name: "Exxon Mobil",
     revenue: 398
   },
-]
+];
 
 
+const filteredCompanies = companies.filter(company => company.revenue > 500);
+console.log(filteredCompanies);
 
 
+console.log("\n\n----- Exercice B : Ménage de printemps -----");
 /* 
     B. Ménage de printemps.
 
     Supprimez toutes les valeurs falsy du tableau "values" ci-dessous.
     Retournez dans la console un tableau "truthyValues" contenant les valeurs restantes.
 */
-
 const values = ["", undefined, "a", "a", 99, 0, true, false, 5, 5, 5];
 
+const truthyValues = values.filter(value => value);
+console.log(truthyValues);
 
-/* 
+
+console.log("\n\n----- Exercice C : Nom de nom -----");
+/*
     C. Nom de nom.
 
     Retournez un nouveau tableau "filteredNames" contenant seulement les prénoms commençant par la lettre A et ayant une longueur minimale de 5.
 */
 
 const names = ["Adrien", "Paul", "Victor", "Alexandre", "Aurélie", "Antoine"];
-
+const filteredNames = names.filter(name => name[0] === "A" && name.length >= 5 );
+console.log(filteredNames);

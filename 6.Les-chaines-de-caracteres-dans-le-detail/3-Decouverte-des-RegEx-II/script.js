2d726e5c3ccd55c0c19d4b935a668ec0ce3cae5b
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-11 10:14:30 +0100

    Ajout du cours sur "Les RegExp - les expressions régulières (ou rationnelles)" suite et fin : - les groupes optionnels ()? - les groupes nommés (?<>) - les ancres : début de chaîne ^ et fin de chaîne $

--- 6.Les-chaines-de-caracteres-dans-le-detail/3-Decouverte-des-RegEx-II/script.js
+++ 6.Les-chaines-de-caracteres-dans-le-detail/3-Decouverte-des-RegEx-II/script.js
@@ -71,7 +71,13 @@ console.log("/a{2,4}/g | -> ", "azzaa,aaaaaaaa a a aa aaa".match(/a{2,4}/g));// 
 */
 console.log("\n\n----- C. Intervalle infini {x,} -----");
 console.log("/a{1,}/g | -> ", "azzaa,aaaaaaaaaaaaaa".match(/a{1,}/g))// Retourne ->
-// [ 'a', 'aa', 'aaaaaaaaaaaaaa' ]
+// [ 'a', 'aa', 'aaaaaaaaaaaaaa' ] | Un tableau de toutes les séries de "a" et les "a" seuls
+//
+console.log("/a{1,}/g | -> ", "Haaa oui ! Ce matin, il fait beau et chaud !".match(/a{1,}/g))// Retourne ->
+// [ 'aaa', 'a', 'a', 'a', 'a' ] | Un tableau de toutes les séries de "a" et les "a" seuls
+
+console.log("/a{1,}/g | -> ", "Ce soir.".match(/a{1,}/g))// Retourne ->
+// null | Car aucune lettre correspondant à "a"
 
 
 /* 
@@ -82,7 +88,13 @@ console.log("/a{1,}/g | -> ", "azzaa,aaaaaaaaaaaaaa".match(/a{1,}/g))// Retourne
 */
 console.log("\n\n----- D. Le signe + -----");
 console.log("/a+/g | -> ", "azzaa,aaaaaaaaaaaaaa".match(/a+/g))// Retourne ->
-// [ 'a', 'aa', 'aaaaaaaaaaaaaa' ]
+// [ 'a', 'aa', 'aaaaaaaaaaaaaa' ] | Un tableau de toutes les séries de "a" et les "a" seuls
+
+console.log("/a+/g | -> ", "Haaa oui ! Ce matin, il fait beau et chaud !".match(/a+/g))// Retourne ->
+// [ 'aaa', 'a', 'a', 'a', 'a' ] | Un tableau de toutes les séries de "a" et les "a" seuls
+
+console.log("/a+/g | -> ", "Ce soir.".match(/a+/g))// Retourne ->
+// null | Car aucune lettre correspondant à "a"
 
 
 /* 
@@ -185,8 +197,11 @@ Transformons le modèle "prénom.nom" en une regex.
 Résultat : /([A-zÀ-ÿ]+).([A-zÀ-ÿ]+)/g
 Le premier groupe contient entre 1 et une infinité de caractères
 */
-console.log("jean.dupont".match(/([A-zÀ-ÿ]+).([A-zÀ-ÿ]+)/g));
-console.log("julie.andré".match(/([A-zÀ-ÿ]+).([A-zÀ-ÿ]+)/g))
+console.log("jean.dupont".match(/([A-zÀ-ÿ]+).([A-zÀ-ÿ]+)/g));// Retourne ->
+// [ 'jean.dupont' ]
+
+console.log("julie.andré".match(/([A-zÀ-ÿ]+).([A-zÀ-ÿ]+)/g))// Retourne ->
+// [ 'julie.andré' ]
 
 
 console.log("\n----- B. Les groupes optionnels -----");
@@ -194,9 +209,42 @@ console.log("\n----- B. Les groupes optionnels -----");
     B. Les groupes optionnels
 
     On peut utiliser le ? pour définir des groupes optionnels.
+
+    Dans l'exemple ci-dessous, on cherche un b qui est suivi du groupe (zb)
+*/
+// On voit ici que le group (zb) et mis en option en ajoutant ?
+// Il est testé sur la chaîne "bzb" qui contient le groupe (zb)
+// Donc match retourne naturellement :
+// Un tableau (Array) qui contient les résultats des correspondances
+// et les groupes capturés grâce aux parenthèses.
+console.log("/b(zb)?/ | -> ", "bzb html bzb css bzb".match(/b(zb)?/))
+/* Retourne ->
+    [
+      'bzb',
+      'zb',
+      index: 0,
+      input: 'bzb html bzb css bzb',
+      groups: undefined
+    ]
 */
 
-    // console.log("bzbaa")
+// En utilisant une fois de plus le drapeau g, on obtient toutes les correspondances :
+console.log("/b(zb)?/g | -> ", "bzb html bzb css bzb".match(/b(zb)?/g))// Retourne ->
+// [ 'bzb', 'bzb', 'bzb' ]
+
+// En faisant le test sur une chaîne contenant uniquement b, le groupe (zb) étant optionnel
+// La correspondance avec b, fonctionne bien sinon on aurait obtenu null :
+console.log("/b(zb)?/ | -> ", "b".match(/b(zb)?/))// Retourne ->
+// [ 'b', undefined, index: 0, input: 'b', groups: undefined ]
+
+// On peut tester avec la chaîne baa et un groupe supplémentaire (aa) cette fois obligatoire
+// mais on garde (zb)? optionnel pour voir. Et voilà, il y a bien une correspondance :
+console.log("/b(zb)?(aa)/ | -> ", "baa".match(/b(zb)?(aa)/))// Retourne ->
+// [ 'baa', undefined, 'aa', index: 0, input: 'baa', groups: undefined ]
+
+// Cette fois, je teste une autre chaîne, mais avec le même groupe pour voir le fameux null :
+console.log("/b(zb)?(aa)/ | -> ", "haa bah oui bab".match(/b(zb)?(aa)/))// Retourne -> null
+// La lettre "b" est bien présente, le groupe (zb) est optionnel, mais le groupe (aa) est absent et obligatoire
 
 
 console.log("\n----- C. Les groupes nommés -----");
@@ -204,23 +252,84 @@ console.log("\n----- C. Les groupes nommés -----");
     C. Les groupes nommés
 
     On peut nommer des groupes afin de s'y retrouver plus facilement, cette méthode est assez peu utilisée.
-
-    Exemple : /(?<nom>x)/
+    La syntaxe expliquée de la regExp :
+    - étant un groupe il y a toujours () et donne /()/
+    - puis démarre avec ? et donne /(?)/
+    - ensuite les chevrons <> qui englobent le nom du groupe que l'on veut donner /(?<nomDuGroupQueJeChoisis>)/
+    - pour finir la regExp x que ce groupe nommé représente /(?<nomDuGroupQueJeChoisis>x)/
+    Exemple du format : /(?<nomDuGroupQueJeChoisis>x)/
 
 */
+// Pour tester un numéro de tel américain par exemple :
+console.log("/(?<USnumber>x)/ | -> ", "911".match(/(?<USnumber>\d\d\d)/))// Retourne ->
+/*
+    [
+      '911',
+      '911',
+      index: 0,
+      input: '911',
+      groups: [Object: null prototype] { USnumber: '911' }
+    ]
+*/
+
+// Ou juste un nombre, peu importe sa longeur :
+console.log("/(?<number>\d+)/ | -> ", "0123456789".match(/(?<number>\d+)/))// Retourne ->
+/*
+    [
+      '0123456789',
+      '0123456789',
+      index: 0,
+      input: '0123456789',
+      groups: [Object: null prototype] { number: '0123456789' }
+    ]
+ */
+
 
 
 console.log("\n\n\n----- 3. Les ancres ^ et $ -----");
 /* 
     3. Les ancres ^ et $
 
-    L'accent circonflexe permet de chercher une expression qui est au début d'une chaîne, et le dollar permet de chercher un modèle qui termine une chaîne.
+    L'accent circonflexe permet de chercher une expression qui est au début d'une chaîne,
+    et le dollar permet de chercher un modèle qui termine une chaîne.
 */
 
     /* A. Un peu de pratique */
-    // console.log("Je suis dans de beaux draps.") // [Je]
-    // console.log(" Je suis dans de beaux draps.") // null (notez l'espace au début)
-    // console.log("Je suis dans de beaux draps.") 
+    // Je cherche "Je" au début de la phrase :
+    console.log("Je suis dans de beaux draps.".match(/^Je/))
+    /*
+        [
+          'Je',
+          index: 0,
+          input: 'Je suis dans de beaux draps.',
+          groups: undefined
+        ]
+     */
 
+    // Je cherche toujours "Je" au début de la phrase :
+    console.log(" Je suis dans de beaux draps.".match(/^Je/))
+    // null (notez l'espace au début)
+
+    // Je cherche "draps." à la fin de la phrase :
+    console.log("Je suis dans de beaux draps.".match(/draps\.$/))
+    /*
+        [
+          'draps.',
+          index: 22,
+          input: 'Je suis dans de beaux draps.',
+          groups: undefined
+        ]
+     */
+
+    // Je cherche un début "Je" et une fin de phrase "draps." à la fois :
+    console.log("Je suis dans de beaux draps.".match(/^Je.*draps\.$/))
+    /*
+        [
+          'Je suis dans de beaux draps.',
+          index: 0,
+          input: 'Je suis dans de beaux draps.',
+          groups: undefined
+        ]
+     */
 
 

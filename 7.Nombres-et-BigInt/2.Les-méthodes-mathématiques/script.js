35b14b1fb18b2c2c66da3c45ec487015ff112750
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-11 14:31:29 +0100

    Ajout de la 7ᵉ section du cours "Les nombres et Bigint"

---
+++ 7.Nombres-et-BigInt/2.Les-méthodes-mathématiques/script.js
@@ -0,0 +1,101 @@
+/* 
+    Un objet Math est disponible en JavaScript.
+
+    Math n'est pas un constructeur(qui sert à créer des objets), mais simplement un objet contenant pléthore de méthodes utiles pour réaliser des calculs mathématiques (sinus, puissance, random, min, max, etc ...).
+
+    Le but n'est pas de toutes les connaître par coeur, mais simplement de savoir qu'elles existent et les utiliser en temps voulu.
+
+    Découvrons les méthodes MATH les plus populaires et utiles.
+*/
+
+/* 
+    1. Math.abs(number)
+
+    Retourne la valeur absolue d'un nombre, c'est à la dire la valeur d'un nombre sans son signe.
+    La valeur absolue de x est x.
+    La valeur absolue de -x est x.
+*/
+
+
+
+/* 
+    2. Math.ceil(number)
+
+    Retourne l'entier suivant un nombre à virgule.
+*/
+
+ 
+
+
+/* 
+    3. Math.floor(number)
+
+    Inverse de .ceil().
+    Retourne l'entier inférieur d'un nombre à virgule.
+*/
+
+
+    
+
+/* 
+    4. Math.round(number)
+
+    Mélange de .ceil et .floor().
+    Si le dernier chiffre d'un nombre à virgule est supérieur ou égal à 5, on arrondit à l'entier supérieur, sinon à l'entier inférieur. 
+*/
+
+
+
+/* 
+    5. Math.cos(numberInRadians)
+
+    Retourne le cosinus d'un angle.
+*/
+
+
+
+/* 
+    6. Math.max(x,y,...)
+
+    Cette méthode retourne le nombre le plus grand parmi les arguments qu'on lui passe.
+*/
+
+
+
+/* 
+    7. Math.min(x,y,...)
+
+    Cette méthode retourne le nombre le plus petit parmi les arguments qu'on lui passe.
+*/
+
+
+/* 
+    8. Math.pow(x,y)
+
+    Retourne le premier argument exposant[second argument].
+*/
+
+
+
+/* 
+    9. Math.random()
+
+    Retourne un nombre "au hasard" entre 0 inclus et 1 non inclus.
+*/
+    // console.log(Math.random());
+    // console.log(Math.random());
+    // console.log(Math.random());
+
+
+/* 
+    10. Math.trunc(number)
+
+    Retour la partie entière d'un nombre à virgule.
+*/
+
+    // console.log(Math.trunc(9.446864)); // 9
+
+/* 
+    Etc etc ...
+    Encore une fois, le but n'est pas de toutes les retenir par coeur, mais de savoir que si vous avez besoin de faire un calcul mathématique en particulier, il existe surement une méthode pour vous aider à le réaliser.
+*/
---
+++ 7.Nombres-et-BigInt/3.Transformer-une-chaîne-en-nombre/script.js
@@ -0,0 +1,18 @@
+/* 
+    Comment transformer une chaîne en nombre ?
+    Voilà un problème sur lequel on peut tomber en JS.
+    Il existe, étrangement, beaucoup de façons de le résoudre.
+*/
+
+    // .parseInt
+
+    // .parseFloat
+
+    // Le constructeur Number(), appelé en tant que fonction, peut convertir une chaîne en nombre.
+
+    // Multiplier avec un nombre
+
+    // Math.floor(), même avec un argument entier.
+
+    // L'opérateur unaire +
+    // L'opérateur unaire (+) précède son opérande et essaye de le convertir en nombre si ce n'est pas déjà une valeur numérique.

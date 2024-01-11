f155ba949ac850f7a86c73aec2c8e4618bff2aec
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-11 11:23:48 +0100

    Ajout du cours sur "Les méthodes liées aux regex" : - .search()? - .replace() - .replaceAll()

--- 6.Les-chaines-de-caracteres-dans-le-detail/4-Les-methodes-liees-aux-regex/script.js
+++ 6.Les-chaines-de-caracteres-dans-le-detail/4-Les-methodes-liees-aux-regex/script.js
@@ -1,5 +1,6 @@
 /* 
-    Après avoir vu les différentes caractéristiques des expressions régulières, découvrons ensemble les méthodes qui les utilisent.
+    Après avoir vu les différentes caractéristiques des expressions régulières,
+    découvrons ensemble les méthodes qui les utilisent.
 */
 
 
@@ -9,22 +10,27 @@
    Retourne l'index de la première correspondance.
 */
 
-   // console.log("En hiver, il fait froid") // 10
+   console.log("En hiver, il fait froid".search(/il/)) // 10
 
 
 /* 
    2. str.replace(str/regex, str/function)
 
    Remplace la première occurrence du premier argument par le second argument.
-   Le remplacement peut être globale ou unique, suivant le /g au niveau du regex.
+   Le remplacement peut être global ou unique, suivant le /g au niveau du regex.
 */
 
-   // console.log("Bo1nj1ou11r l1e m111onde") // Bonj1ou11r l1e m111onde
-   // console.log("Bo1nj1ou11r l1e m111onde") // Bonjour le monde 
-   // console.log("Bo1nj1ou11r l1e m111onde")  // Bonj1ou11r l1e m111onde
+    console.log("Bo1nj1ou11r l1e m111onde".replace("1",""))
+    // Bonj1ou11r l1e m111onde
 
-   // console.log("J'adore les chiens") // "J'adore les chats"
+    console.log("Bo1nj1ou11r l1e m111onde".replace(/1/,""))
+    // Bonjour le monde
 
+    console.log("Bo1nj1ou11r l1e m111onde".replace(/1/g,""))
+    // Bonj1ou11r l1e m111onde
+
+    console.log("J'adore les chiens".replace("chiens","chats"))
+    // "J'adore les chats"
 
 /* 
    3. str.replaceAll(str/regex, str/func)
@@ -32,4 +38,5 @@
    Remplace toutes les occurences représentées par le premier argument par la chaîne fournie en second argument.
 */
 
-   // console.log("55-55-55-55-55") // '11-11-11-11-11'
+   console.log("55-55-55-55-55".replaceAll("55","11")) // '11-11-11-11-11'
+   console.log("55-55-55-55-55".replaceAll(/55/g,"11")) // '11-11-11-11-11'

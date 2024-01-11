41176f3327709e56969817bff2cb3767cb3b273a
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-11 18:50:17 +0100

    Ajout du cours sur "Les très grand nombre type BigInt"

--- 7.Nombres-et-BigInt/4.Le-type-BigInt/script.js
+++ 7.Nombres-et-BigInt/4.Le-type-BigInt/script.js
@@ -1,10 +1,15 @@
 /* 
     Récemment, JavaScript a rajouté le type BigInt qui permet de manipuler de très grands nombres.
     Il faut rajouter la lettre n minuscule à la fin d'un bigInt afin que JavaScript l'interprète correctement.
-    Les BigInt representent des nombres supérieurs à (2^53) - 1
+    Les BigInt représentent des nombres supérieurs à (2^53) - 1
 */
 
 
 const bigIntNumber1 = 676878678676876877n;
 const bigIntNumber2 = 976878678676876877n;
 
+console.log(typeof bigIntNumber1) // bigint
+console.log(typeof 55) // number
+
+console.log(bigIntNumber1 > bigIntNumber2) // false
+console.log(bigIntNumber1 < bigIntNumber2) // true

df11ffed5fbfa06a84d35ed511f57237a66dd1ae
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-15 23:19:56 +0100

    Ajout exercices sur "Nombre de jours entre deux dates"

--- 8.Les-Dates/Exercices/1-Nombre-de-jours-entre-deux-dates/script.js
+++ 8.Les-Dates/Exercices/1-Nombre-de-jours-entre-deux-dates/script.js
@@ -7,7 +7,7 @@
 */
 
 function daysBetweenDates(firstDate, secondDate){
-
+    return Math.trunc(Math.abs((Math.abs(secondDate) - Math.abs(firstDate))) / (24*60*60*1000))
 }
-console.log(daysBetweenDates(new Date(2021,0,13), new Date(2021,0,22)))
-console.log(daysBetweenDates(new Date(1021,0,13), new Date(2021,0,22)))
+console.log(daysBetweenDates(new Date(2021,0,13), new Date(2021,0,22))) // 9
+console.log(daysBetweenDates(new Date(1021,0,13), new Date(2021,0,22))) // 327954

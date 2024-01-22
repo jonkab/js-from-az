7e661c8baa3a65e86940830e60884a0912822c85
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-22 17:13:28 +0100

    Ajout du cours sur "La sélection d'éléments du DOM ainsi que ses objets et méthodes associées partie 1 : - l'objet window - l'objet document - querySelector() - querySelectorAll()

--- 9.Le-Document-Object-Model/1.La-selection-en-javascript/script.js
+++ 9.Le-Document-Object-Model/1.La-selection-en-javascript/script.js
@@ -1,6 +1,21 @@
 /* 
     Les méthodes de sélection sont disponibles via l'objet document, mais aussi via tous les objets représentant des éléments. 
 */
+// L'objet "window" représente l'objet global de la fenêtre du navigateur
+console.log(window)
+
+// L'objet "document" inclut dans l'objet global "window",
+console.log(window.document)// représente le document(la page) ouvert dans la fenêtre du navigateur
+
+// Étant déjà directement dans l'objet global "window",
+console.log(document)// on peut constater que l'on peut accéder directement à l'objet "document" sans écrire "window" avant.
+
+/*
+    Ce qui explique pour quoi tous les objets, méthodes ou autre comme "console", "alert()" ou autre sont
+    également accéssible directement sans devoir repasser par l'objet "window", car nous sommes en réalité
+    déjà dedans.
+*/
+
 
 /* 
     1 document|element.querySelector(selecteurCSS)
@@ -9,7 +24,11 @@
 
     Retourne un objet représentant le premier élément trouvé.
 */
-
+const mainTitle = document.querySelector("h1")
+console.log(mainTitle)
+console.log(typeof mainTitle)
+console.log(mainTitle.style.backgroundColor = "crimson")
+console.log(mainTitle.style.fontSize = "40px")
 
 
 
@@ -19,13 +38,21 @@
     Méthode "récente" permettant de sélectionner plusieurs noeuds du DOM. 
     Un noeud de DOM peut être du texte, un commentaire, ou encore un élément.
     
-    Retourne une nodelist statique.
+    Retourne une NodeList statique.
 
-    Le fait qu'elle soit statique signifie qu'elle ne réagit pas à des changements ultérieurs sur les éléments qu'elle contient, elle reste toujours la même.
+    Le fait qu'elle soit statique signifie qu'elle ne réagit pas à des changements ultérieurs
+    sur les éléments qu'elle contient, elle reste toujours la même c'est donc une copie unique.
+
+    NodeList est un objet représenté sous forme de tableau, mais avec moins de méthode que les objets natif "Array"
 */
-    
+const listElement = document.querySelectorAll("ul li")
+console.log("NodeList statique :", listElement)
+listElement.forEach((el, index) => el.innerText = `Changed ${index + 1} !`)
 
-
+setTimeout(() => {
+    document.querySelector(".list-item:nth-child(3)").remove()
+    console.log("NodeList statique toujours identique malgré la suppression du 3éme item de la list :", listElement)
+}, 1000)
 
 /* 
     3 .getElementsByClassName(strClassName)

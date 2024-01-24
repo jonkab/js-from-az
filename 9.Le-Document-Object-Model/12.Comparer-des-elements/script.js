191f52b3d65c323152333ddb6c886e1cdd64c56e
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-24 19:04:47 +0100

    Ajout du cours sur "Comparer des éléments (valeur type référence)"

--- 9.Le-Document-Object-Model/12.Comparer-des-elements/script.js
+++ 9.Le-Document-Object-Model/12.Comparer-des-elements/script.js
@@ -6,3 +6,12 @@
     Si deux objets ont la même référence, ils représentent le même objet dans la mémoire.
 */
 
+const secondItem = document.querySelector(".item-2")
+const secondLi = document.querySelector("#item-2")
+
+console.log(secondItem === secondLi)// true
+
+const obj1 = {name: "John"}
+const obj2 = obj1
+
+console.log(obj1 === obj2)// true

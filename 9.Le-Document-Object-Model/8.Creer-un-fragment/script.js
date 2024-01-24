07a5eab527d0eee97f735d7897fdc0a4b46541fe
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-24 17:23:04 +0100

    Ajout du cours sur "Créer un fragment avec la méthode document.createDocumentFragment()"

--- 9.Le-Document-Object-Model/8.Creer-un-fragment/script.js
+++ 9.Le-Document-Object-Model/8.Creer-un-fragment/script.js
@@ -5,3 +5,17 @@
     Les éléments contenus dans le fragment se rajoutent tout simplement dans l'élément souhaité. 
 
 */
+const textTitle1 = "Lorem ipsum dolor."
+const textTitle2 = "Sit amet."
+
+const fragment = document.createDocumentFragment()
+
+const h1 = document.createElement("h1")
+h1.textContent = textTitle1
+fragment.appendChild(h1)
+
+const h2 = document.createElement("h2")
+h2.textContent = textTitle2
+fragment.appendChild(h2)
+
+document.body.appendChild(fragment)

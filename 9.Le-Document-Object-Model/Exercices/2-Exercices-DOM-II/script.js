d210fa5e2cb1537407bdc853843239e2564dbac1
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-25 15:37:55 +0100

    Ajout exercices sur "DOM II"

--- 9.Le-Document-Object-Model/Exercices/2-Exercices-DOM-II/script.js
+++ 9.Le-Document-Object-Model/Exercices/2-Exercices-DOM-II/script.js
@@ -26,13 +26,29 @@ const data = [
 
     Un <li> par objet doit être créé.
 
-    Chaque <li> contiendra un <p> avec le contenu de la propriété .content et un autre <p> avec le contenu de la propriété .category.
+    Chaque <li> contiendra un <p> avec le contenu de la propriété .content et
+    un autre <p> avec le contenu de la propriété .category.
 
     Ajoutez ensuite chaque <li> dans la liste <ul>
 */
-
-
+function fillList(list, data) {
+    data.forEach(obj => {
+        const li = document.createElement("li")
+        const liContent = `
+            <p class="content"></p>
+            <p class="category"></p>
+        `
+        li.innerHTML = liContent
+        li.querySelector(".content").textContent = obj.content
+        li.querySelector(".category").textContent = obj.category
+        list.appendChild(li)
+    })
+}
+const list = document.querySelector(".list")
+fillList(list, data)
 
 /*
     2. Supprimez maintenant le premier et dernier élément de la liste.
 */
+list.removeChild(document.querySelector("li:first-child"))
+list.removeChild(document.querySelector("li:last-child"))

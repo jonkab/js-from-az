19ab136ac193c7670dac196d55310d5a62b58bdd
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-25 19:25:51 +0100

    Ajout du cours sur "Introduction aux évènements" avec les méthodes : - addEventListener(event, callback, option) - removeEventListener(type, callbackListener) - onclick() - l'objet event

--- 10.Les-evenements/1.Introduction-aux-evenements/script.js
+++ 10.Les-evenements/1.Introduction-aux-evenements/script.js
@@ -1,19 +1,33 @@
 /* 
-    Les événements sont au coeur du JavaScript côté navigateur, ils permettent de répondre à des actions effectuées par les utilisateurs.
+    Les événements sont au cœur du JavaScript côté navigateur, ils permettent de répondre
+    à des actions effectuées par les utilisateurs.
 */
 
 /* 
     1. .addEventListener(event, callback, option)
     
-    Elle permet d'être à l'écoute d'un évenement particulier sur un élément, c'est à dire de déclencher une fonction callback quand un évènement a été déclenché sur un élément.
+    Elle permet d'être à l'écoute d'un évènement particulier sur un élément, c'est
+    à dire de déclencher une fonction callback quand un évènement a été déclenché
+    sur un élément.
 */
-    
+    const btn = document.querySelector(".btn")
 
+    //btn.addEventListener("click", handleClick)
+    btn.addEventListener("click", handleClick2)
+    function handleClick() {
+        console.log("Clicked !")
+    }
+
+    function handleClick2(e) {
+        console.log("Clicked ! 2")
+        console.log(e)
+    }
 
 /* 
     L'objet d'évènement.
 
-    Un objet d'évènement contenant des informations de ce dernier est TOUJOURS passé en argument à la fonction callback que vous définissez dans addEventListener().
+    Un objet d'évènement contenant des informations de ce dernier est TOUJOURS
+    passé en argument à la fonction callback que vous définissez dans addEventListener().
 
     À vous de voir si vous voulez l'utiliser, ou pas.
 
@@ -24,10 +38,19 @@
     .removeEventListener(type, callbackListener)
 
     Enlève un écouteur d'évènement d'un certain élément.
-    Cela permet, dans certains cas, d'alléger le fonctionnement d'une application et d'avoir de meilleures performances.
+    Cela permet, dans certains cas, d'alléger le fonctionnement d'une application et
+    d'avoir de meilleures performances.
 
-    Attention, on ne peut qu'enlever des écouteurs d'évènements rajoutés avec la méthode addEventListnerner ET lorsque on a passé une fonction nommée et pas une fonction anonyme.
+    Attention, on ne peut qu'enlever des écouteurs d'évènements rajoutés avec
+    la méthode addEventListnerner ET lorsqu'on a passé une fonction nommée
+    et pas une fonction anonyme.
 */
+
+btn.removeEventListener("click", handleClick)
+
+//Ici la fonction étant anonyme, je ne pourrai jamais la supprimer avec removeEventListener()
+btn.addEventListener("click", () => { console.log("Fonction anonyme")})
+
 
 
 /* 
@@ -40,8 +63,11 @@
     - On ne peut pas les enlever
     - On ne peut pas utiliser les paramètres supplémentaires d'addEventListeners()
 */
-
+//btn.onclick = handleClick
 
 /* 
     Rajouter des évènements en HTML.
+
+    On peut voir que ne pouvant pas utiliser les paramètres supplémentaires "le (e)" d'addEventListeners()
+    Dans la console ça renvoie "undefined"
 */

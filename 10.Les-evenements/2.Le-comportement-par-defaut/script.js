01bb245cdeb9bf5ddfc36bb5ac3359da3a9a73f3
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-26 10:36:15 +0100

    Ajout du cours sur "Le comportement par défaut" comment le stopper avec les méthodes : - addEventListener(event, callback, option) - preventDefault() Par exemple au "click" sur des liens ou au "submit" pour un formulaire

--- 10.Les-evenements/2.Le-comportement-par-defaut/script.js
+++ 10.Les-evenements/2.Le-comportement-par-defaut/script.js
@@ -1,11 +1,37 @@
 /* 
-    Certains éléments ont déjà des écouteurs d'évènement par défaut et vont donc avoir un comportement spécial quand on les déclenche.
+    Certains éléments ont déjà des écouteurs d'évènement par défaut et
+    vont donc avoir un comportement spécial quand on les déclenche.
 
     Les formulaires vont tenter d'envoyer les données vers une page.
 
     Les liens nous font nous déplacer au click.
 
-    Parfois, on a envie de prévenir ces comportements par défaut, on utilisera alors EventObject.preventDefault().
+    Parfois, on a envie de prévenir ces comportements par défaut,
+    on utilisera alors EventObject.preventDefault().
+
+    C'est à dire que l'on bloquera le comportement par défaut de l'évènement en question.
 
     Testons tout ça...
 */
+
+/*
+*   Les liens et leurs redirections
+* */
+const links = document.querySelectorAll("a")
+console.log(links)
+links.forEach(link => link.addEventListener("click", handleClick))
+function handleClick(e) {
+    e.preventDefault()
+    console.log("Redirection de la page stoppée.")
+}
+
+
+/*
+*   Les formulaires et leurs soumissions de données
+* */
+const form = document.querySelector("form")
+form.addEventListener("submit", handleSubmit)
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log("Soumission du formulaire stoppée.")
+}

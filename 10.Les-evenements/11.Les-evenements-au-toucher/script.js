772596ac37e763c894dd9974842bfc104d5c8213
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-29 17:39:56 +0100

    Ajout du cours sur "Les events au toucher" : - touchstart - touchend - touchmove - click (fonctionne aussi en mode simulation mobile responsive)

--- 10.Les-evenements/11.Les-evenements-au-toucher/script.js
+++ 10.Les-evenements/11.Les-evenements-au-toucher/script.js
@@ -1,4 +1,17 @@
 /*
     Les "touch events" permettent de prendre en compte le "clic" d'un doigt sur un écran tactile.
+
+    À tester en mode responsive grâce aux options fournies dans la DevTools du navigateur.
+
+    Un point sera afficher pour remplacer le curseur de la souris afin de simuler un doigt.
 */
+
+document.addEventListener("touchstart", () => console.log("touchstart"))
+
+document.addEventListener("touchend", () => console.log("touchend"))
+
+document.addEventListener("touchmove", () => console.log("touchmove"))
+
+//À noter que l'event "click" fonctionne aussi dans le mode simulation mobile responsive
+document.addEventListener("click", () => console.log("click"))
 

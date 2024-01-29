9e3f3759d8be235e3b555acd7d3f083d34e7d3af
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-29 15:15:17 +0100

    Ajout du cours sur "Le class toggle et manipulation de classes CSS" avec les propriétés et méthodes sur l'interface DOMTokenList contenant classList: - toggle() - add(classe) - remove(classe) - contains(classe) - toggle(classe) - replace(classeÀRemplacer, classeÀRajouter) - .className

--- 10.Les-evenements/6.Le-class-toggle/script.js
+++ 10.Les-evenements/6.Le-class-toggle/script.js
@@ -1,11 +1,17 @@
 /*
-  Une des actions les plus courantes avec les évènements est de faire un "class toggle", c'est à dire un changement de classe html.
+  Une des actions les plus courantes avec les évènements est de faire un "class toggle", c'est-à-dire un changement de classe html.
 
   Le fait de changer une classe permet tout simplement d'altérer le style d'un élément en question.
   
   C'est la porte ouverte à une infinité d'animations et d'interactions possibles.
 */
+const box = document.querySelector(".box")
 
+box.addEventListener("click", toggleAnimation)
+
+function toggleAnimation(e){
+    e.target.classList.toggle("active")
+}
 
 
 /*
@@ -13,10 +19,12 @@
   C'est un ensemble de méthodes pratiques concernant les classes. 
   
   Il existe : 
-  - add(classe)
-  - remove(classe)
+  - add(classe) // Ajoute une ou plusieurs classes passées en argument
+  - remove(classe) // retire la classe s'il la trouve
+  - contains(classe) // retourne un boolean s'il trouve ou non la classe
   - toggle(classe) // classe supprimée si présente, rajoutée si absente
   - replace(classeÀRemplacer, classeÀRajouter)
 
   On peut aussi changer une classe en utilisant la propriété .className des éléments du DOM.
 */
+

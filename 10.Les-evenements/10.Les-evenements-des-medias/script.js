72598f1a48062a4e30ccfaf2d50a5104661f27fe
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-29 17:32:16 +0100

    Ajout du cours sur "Les events d'un des médias (audio et vidéo)" : - loadeddata - play - pause - timeupdate - volumechange

--- 10.Les-evenements/10.Les-evenements-des-medias/script.js
+++ 10.Les-evenements/10.Les-evenements-des-medias/script.js
@@ -1,9 +1,20 @@
 /* 
     On peut intégrer de la musique et des vidéos à l'aide des éléments <audio> et <video>.
-    Ces élèments peuvent réagir à énormement d'évènements en rapport avec ces éléments.
+    Ces élèments peuvent réagir à énormément d'évènements en rapport avec ces éléments.
 
     Découvrons ensemble : pause, play, loadeddata, playing, progress, etc...
 */
+    const video = document.querySelector("video")
+
+    video.addEventListener("loadeddata", () => console.log("Data loaded"))
+
+    video.addEventListener("play", () => console.log("play"))
+
+    video.addEventListener("pause", () => console.log("pause"))
+
+    video.addEventListener("timeupdate", () => console.log("timeupdate"))
+
+    video.addEventListener("volumechange", () => console.log("volumechange"))
 
 
 
@@ -11,3 +22,14 @@
     Même combat pour l'élément <audio>.
 */
 
+    const audio = document.querySelector("audio")
+
+    audio.addEventListener("loadeddata", () => console.log("Data loaded"))
+
+    audio.addEventListener("play", () => console.log("play"))
+
+    audio.addEventListener("pause", () => console.log("pause"))
+
+    audio.addEventListener("timeupdate", () => console.log("timeupdate"))
+
+    audio.addEventListener("volumechange", () => console.log("volumechange"))

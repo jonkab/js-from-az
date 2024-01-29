84ec7f94979acbf203c1a7673b9e6e32c6af17af
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-01-29 16:06:42 +0100

    Ajout du cours sur "Le events Scroll du clavier" et les propriétés : - keydown - keyup - event.key

--- 10.Les-evenements/8.Les-evenements-de-clavier/script.js
+++ 10.Les-evenements/8.Les-evenements-de-clavier/script.js
@@ -1,3 +1,12 @@
 /* 
     Les évènements de clavier peuvent nous permettre de connaître les touches pressées, ce qui peut être très utile dans de nombreux cas.
 */
+const keyboardInfo = document.querySelector(".keyboard-info")
+
+//document.addEventListener("keydown", handleKey)
+document.addEventListener("keyup", handleKey)
+
+function handleKey(e){
+    console.log(e)
+    keyboardInfo.textContent = `${e.key}`
+}

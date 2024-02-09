8ac75f9dc30a2ac6fd62ddf2e8a5e21d0e86db94
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-02-09 14:02:30 +0100

    Ajout du cours sur "Astuces objet ES6 lié aux nouvelles fonctionnalités syntaxiques ajoutées aux objets" : - Nouvelle syntaxe d'une fonction classique dans un objet - Syntaxe d'une fonction fléchée dans un objet - Nouvelle manière d'ajouter des propriétés facilement à un objet - Utiliser une expression pour créer une propriété dans un objet

--- 13.Le-JavaScript-moderne/5.Les-objets-ES6/script.js
+++ 13.Le-JavaScript-moderne/5.Les-objets-ES6/script.js
@@ -3,24 +3,38 @@
 */
 
 //  Rajouter une fonction classique
-
+const obj = {
+  oldVersion: function () {
+    //Ancienne syntaxe datant d'avant 2025
+  },
+  newVersion() {
+    //Nouvelle syntaxe de 2015, plus besoin des 2 point ":" ni du mot clé "function"
+  },
+  arrow: () => {
+    //Ainsi que la fonction fléchée sans le mot clé "function"
+  }
+}
 
 
 
 // Rajouter des propriétés facilement.
-
 const userName = "Lucie";
 const age = 24;
 
 // création de propriété
 const user = {
+  userName,
+  age
 }
+console.log(user)//{ userName: 'Lucie', age: 24 }
+
 
 
 
 // Utiliser une expression pour créer une prop
-
 let category = "industry";
 const plant = {
   id: 547,
+  [category]: "Microship"
 }
+console.log(plant)

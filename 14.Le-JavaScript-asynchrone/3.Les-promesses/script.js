80d5a5e26201a5991ed9aa79f10f09c4c5209b47
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-02-14 10:31:54 +0100

    Ajout du cours sur "Les promesses (Promise) et ses méthodes" : - l'objet Promise - resolve() - reject() - then() - catch() - finally()

--- 14.Le-JavaScript-asynchrone/3.Les-promesses/script.js
+++ 14.Le-JavaScript-asynchrone/3.Les-promesses/script.js
@@ -3,7 +3,7 @@
 */
 
 /* 
-  On commence par instancier (créer un objet à partir d'un constructeur) une nouvelle promessse.
+  On commence par instancier (créer un objet à partir d'un constructeur) une nouvelle promesse.
   
   Cet objet instancié prend une callback avec deux arguments.
   
@@ -13,3 +13,24 @@
 
   Si c'est un échec, on appelle reject qui nous permet d'exécuter une méthode catch, elle aussi disponible dans le prototype des promesses.
 */
+const promise = new Promise((resolve, reject) => {
+    const calculation = 5 + 5;//10
+    //resolve(calculation);
+    reject("Donnée non trouvée");// Va retourner le message dans catch()
+})
+.then(value => {//Toutes les méthodes then() vont pouvoir s'enchainer autant de fois qu'il y en a
+    console.log(value);//10
+    return value * 10;
+})
+.then(value => {//then() s'enchaine à nouveau
+    console.log(value);//100
+    return value * 555;
+})
+.catch((e) => {//catch() récupère avec (e) le message de la méthode reject()
+    console.log(e);//pour l'afficher si besoin : "Donnée non trouvée"
+})
+.finally(() => {//finally() s'exécute même si une erreur est rencontré
+    console.log("Je suis tout le temps exécuté");
+});
+
+console.dir(promise);//log l'objet Promise avec son Proto sa State et son Result.

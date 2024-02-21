665a0cab11ac6e3d38c53a8b09bce1312968dc12
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-02-21 20:16:19 +0100

    Ajout exercices sur "Créer une classe" : 1. Créez une classe 'YoutubeChannel' 2. Créer 2 méthodes dans son prototype.

--- 15.Prototype-et-classes/Exercices/2.Exercices-Classes/script.js
+++ 15.Prototype-et-classes/Exercices/2.Exercices-Classes/script.js
@@ -7,8 +7,30 @@
     3. channelType (array)
 
     Cette classe intègre également deux méthodes dans son prototype :
-    1. 'deleteAcount', qui console.log("Channel deleted")
+    1. 'deleteAccount', qui console.log("Channel deleted")
     2. 'renameChannel', qui peut changer le nom de la chaîne 
 
 */
 
+class YoutubeChannel {
+    constructor(channelName, totalViews, channelType) {
+        this.channelName = channelName;
+        this.totalViews = totalViews;
+        this.channelType = channelType;
+    }
+
+    deleteAccount() {
+        console.log("Channel deleted");
+    }
+
+    renameChannel(newName) {
+        this.channelName = newName;
+    }
+
+}
+const channel = new YoutubeChannel("Informatique", 1000, "Tech");
+console.log(channel);
+
+channel.renameChannel("DevFront");
+console.log(channel);
+console.log(channel.deleteAccount());

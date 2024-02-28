99c052688227f3a69fc2a631758f3a111d92de23
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-02-28 15:31:22 +0100

    Ajout exercice 3 (perso) "Afficher des images d'une API dans un slider" : - [DONE] Créer un slider fonctionnel avec des images de base en local - [DRAFT.OK] Remplacer les images pour les créer dynamiquement et les appeler depuis l'API "The Cat API"

--- 14.Le-JavaScript-asynchrone/Exercices/3.Afficher-des-images-d.API-dans-un-slider/script.js
+++ 14.Le-JavaScript-asynchrone/Exercices/3.Afficher-des-images-d.API-dans-un-slider/script.js
@@ -1,26 +1,81 @@
 
 
 /*
+    Créer les images
+*/
+const jsonTheCatAPI = "https://api.thecatapi.com/v1/images/search?limit=10";
+const slidesElt = document.querySelector(".slides");
+
+async function getCATImg(url) {
+    let imgData;
+    const errorMsgElt = document.querySelector(".error-msg");
+
+    try {
+        const response = await fetch(url);
+        const headers = response.headers.get("Content-Type");
+
+        if (!response.ok) throw Error(`${response.status}`);
+        if (!headers || !headers.includes("application/json")) throw Error("Les données ne sont pas du JSON.");
+
+        imgData = await response.json();
+        console.log("imgData : ", imgData);
+    }
+    catch (error) {
+        //console.dir(error);
+        errorMsgElt.classList.add("active");
+        errorMsgElt.textContent = `Voici l'erreur rencontré : ${error.message}`;
+    }
+
+    if (imgData) {
+        creatImg(imgData);
+        slider();
+    }
+}
+
+getCATImg(jsonTheCatAPI);
+
+
+function creatImg(data) {
+    data.forEach((image, index) => {
+        const imageElt = document.createElement("img");
+
+        imageElt.className = "slider-img";
+        imageElt.src = data[index].url;
+        /*
+        divItemElt.innerHTML = `
+            <h2 class="title"></h2>
+            <a href="#">Lire l'image</a>
+        `;
+        divItemElt.querySelector(".title").textContent = `${image.title}`;
+*/
+        slidesElt.appendChild(imageElt);
+    });
+}
+
+
+/*
     Code slider
 */
+function slider () {
+    const btnScroll = document.querySelectorAll(".btn-scroll");
+    const images = document.querySelectorAll(".slider-img");
+    console.log("images : ", images);
 
-const btnScroll = document.querySelectorAll(".btn-scroll")
-const images = document.querySelectorAll(".slider-img")
+    btnScroll.forEach(btn => btn.addEventListener("click", handleClickBtnScroll));
+    let index = 0;
+    function handleClickBtnScroll(e) {
+        //console.log(e.target.getAttribute("data-action"))
+        index = index + Number(e.target.getAttribute("data-action"));
 
-btnScroll.forEach(btn => btn.addEventListener("click", handleClickBtnScroll))
-let index = 0
-function handleClickBtnScroll(e) {
-    //console.log(e.target.getAttribute("data-action"))
-    index = index + Number(e.target.getAttribute("data-action"))
+        if (index < 0) {
+            index = images.length - 1;
+        }
+        else if (index > images.length - 1) {
+            index = 0;
+        }
 
-    if (index < 0) {
-        index = images.length - 1
+        images[index].scrollIntoView({
+            behavior: "smooth"
+        });
     }
-    else if (index > images.length - 1) {
-        index = 0
-    }
-
-    images[index].scrollIntoView({
-        behavior: "smooth"
-    })
 }

2df8a5658312ba424f64833ece1627c000348f70
Jonathan Kabrit <jonathan.kabrit@gmail.com>
2024-04-25 00:11:21 +0200

    Les boucles : - Ajout de la structure HTML/CSS du tuto - Ajout de la partie "La boucle While" - Ajout d'un entrainement avec for/for...in/for...of
    TODO :
    - Ajouter la partie HTML pour "do while"
    - Ajouter la partie HTML pour "for"
    - Ajouter la partie HTML pour "for...in et for...of"

---
+++ les-boucles/app.js
@@ -0,0 +1,119 @@
+// Boucle For
+const list = document.querySelector('.list');
+const notes = [12, 20, 16, 14];
+
+// en manipulant le DOM
+for (let i = 0; i < notes.length; i++) {
+    const li = document.createElement('li');
+    const liContent = notes[i];
+
+    li.textContent = liContent;
+    list.appendChild(li);
+}
+
+//-----------------------------------
+
+
+console.log(`
+// For...of
+`);
+const person = {
+    firstname : "John",
+    lastname : "Doe",
+    showNotes(arg) {
+        for (let note of arg) {
+            console.log(note);
+        }
+    }
+}
+person.showNotes(notes);
+
+
+console.log(`
+// Dans une fonction
+`);
+function tableNotes(arg) {
+    for (let note of arg) {
+        console.log(note);
+    }
+}
+
+tableNotes(notes);
+
+
+console.log(`
+// For...in
+`);
+for (let key in person) {
+    console.log(person[key]);
+}
+
+
+console.log(`
+// Exercice pour utiliser toutes les boucles possible sur le même objet Array - "Register"
+// Avec une boucle for
+`);
+const registerData = ["John", "Luc", "Sam", "Eric"];
+const registerForElt = document.querySelector('.registerFor');
+
+function registerWithLoopFor (data, element){
+    for (let i = 0; i < data.length; i++) {
+        const liElt = document.createElement('li');
+        const liContent = data[i];
+
+        liElt.textContent = liContent;
+        element.appendChild(liElt);
+    }
+}
+
+registerWithLoopFor(registerData, registerForElt);
+
+
+
+console.log(`
+// Register avec une boucle for...of
+`);
+const registerForOfElt = document.querySelector('.registerForOf');
+function registerWithForOf(arg, element) {
+    for (const argElement of arg) {
+        const li = document.createElement('li');
+        const liContent = argElement;
+
+        li.textContent = liContent;
+        element.appendChild(li);
+    }
+}
+
+registerWithForOf(registerData, registerForOfElt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
